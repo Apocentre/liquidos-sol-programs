10b4190e899219ebc7850f503346c4d428d54cee
ppoliani <ppoliani@gmail.com>
2024-05-16 17:15:47 +0300

    feat: add palceholder for the curve functions

--- programs/onlyfun/src/account_data/bonding_curve.rs
+++ programs/onlyfun/src/account_data/bonding_curve.rs
@@ -1,13 +1,31 @@
 
 use std::mem::size_of;
 use anchor_lang::prelude::*;
+use rust_decimal::prelude::*;
 
 pub const MAX_OPERATORS: usize = 5;
 
 #[account]
-pub struct BondingCurve {}
+pub struct BondingCurve {
+  /// Total supply of the token
+  pub total_supply: u64,
+  /// The balance of reserve token i.e. SOL
+  pub reserve_token_balance: u64,
+}
 
 impl BondingCurve {
   pub const MAX_SIZE: usize = 8
   + size_of::<Self>();
+
+  /// Calculates the number of tokens to mint based on the given amount of reserve tokens.
+  /// This function is used when user buys the token with SOL
+  pub fn calculate_purchase_return() -> Decimal {
+    todo!()
+  }
+
+  /// Given an amount of tokens, calucates the amount of reserve tokens to be sent back.
+  /// This function is used when user sells the tokens and receives back SOL
+  pub fn calculate_sale_return() -> Decimal {
+    todo!()
+  }
 }
