e4592eb2a661ca9c126072e1a5fa3b95ee058e3e
ppoliani <ppoliani@gmail.com>
2024-05-16 18:11:18 +0300

    feat: implement calculate_sale_return

--- programs/onlyfun/src/account_data/bonding_curve.rs
+++ programs/onlyfun/src/account_data/bonding_curve.rs
@@ -21,15 +21,15 @@ impl BondingCurve {
 
   /// Calculates the number of tokens to mint based on the given amount of reserve tokens.
   /// This function is used when user buys the token with SOL
-  pub fn calculate_purchase_return(&self, tokens_received: u64) -> Result<Decimal> {
+  pub fn calculate_purchase_return(&self, reserve_tokens_received: u64) -> Result<Decimal> {
     let a = dec!(3.34315523).safe_mul(dec!(10).safe_powd(dec!(-9))?)?;
     let b = dec!(17.5970429);
     let c = dec!(299215564.8);
     let total_supply: Decimal = self.total_supply.into();
     let d = Decimal::E.safe_powd(a.safe_mul(total_supply)?.safe_sub(b)?)?;
-    let tokens_received: Decimal = tokens_received.into();
+    let reserve_tokens_received: Decimal = reserve_tokens_received.into();
     
-    let k = tokens_received.safe_div(c)?
+    let k = reserve_tokens_received.safe_div(c)?
     .safe_add(d)?
     .safe_ln()?
     .safe_add(b)?
@@ -42,7 +42,17 @@ impl BondingCurve {
 
   /// Given an amount of tokens, calucates the amount of reserve tokens to be sent back.
   /// This function is used when user sells the tokens and receives back SOL
-  pub fn calculate_sale_return() -> Decimal {
-    todo!()
+  pub fn calculate_sale_return(&self, tokens_sold: u64) -> Result<Decimal> {
+    let a = dec!(3.34315523).safe_mul(dec!(10).safe_powd(dec!(-9))?)?;
+    let b = dec!(17.5970429);
+    let c = dec!(299215564.8);
+    let total_supply: Decimal = self.total_supply.into();
+    let tokens_sold: Decimal = tokens_sold.into();
+    let d = Decimal::E.safe_powd(a.safe_mul(total_supply.safe_sub(tokens_sold)?.safe_sub(b)?)?)?;
+    let e = Decimal::E.safe_powd(a.safe_mul(total_supply)?.safe_sub(b)?)?;
+
+    let reserve_tokens_returned = c.safe_mul(d.safe_sub(e)?)?;
+
+    Ok(reserve_tokens_returned) 
   }
 }
