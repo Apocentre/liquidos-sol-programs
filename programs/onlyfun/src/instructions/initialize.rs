72bd48f6c1663924660537a358a3f652d678a3f7
ppoliani <ppoliani@gmail.com>
2024-05-16 15:54:28 +0300

    feat: add cpi_authority pda to the initialize account

--- programs/onlyfun/src/instructions/initialize.rs
+++ programs/onlyfun/src/instructions/initialize.rs
@@ -11,6 +11,16 @@ pub struct Initialize<'info> {
   )]
   pub state: Account<'info, State>,
 
+  /// CHECK: The PDA that will be sending CPI to other programs i.e. Raydium DEX Program
+  #[account(
+    init,
+    payer = owner,
+    space = 0,
+    seeds = [b"cpi_authority", state.key().as_ref()],
+    bump,
+  )]
+  pub cpi_authority: AccountInfo<'info>,
+  
   #[account(mut)]
   pub owner: Signer<'info>,
   
