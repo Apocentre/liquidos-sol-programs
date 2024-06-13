5f81512a57abf4fa005275bb70a67668afb35e26
ppoliani <ppoliani@gmail.com>
2024-06-13 13:31:16 +0300

    fix: buy script

--- scripts/sell.js
+++ scripts/sell.js
@@ -4,7 +4,7 @@ import Web3Pkg, {spl} from "@apocentre/solana-web3";
 import {provider} from "./helpers/provider.js";
 import {createAndSendV0Tx} from "./helpers/tx.js";
 import config from "./config.json" assert { type: "json" };
-import sellerKey from "../wallets/deployer.json" assert { type: "json" };
+import sellerKey from "../wallets/test/buyer1.json" assert { type: "json" };
 
 const Web3 = Web3Pkg.default;
 const {BN} = anchor.default;
