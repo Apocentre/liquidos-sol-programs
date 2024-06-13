fbefdae373d8e9c088a2566d2b70326838652cac
ppoliani <ppoliani@gmail.com>
2024-06-13 10:40:32 +0300

    feat: add the create tax token ix definition

---
+++ programs/onlybags/src/instructions/create_tax_token.rs
@@ -0,0 +1,55 @@
+use anchor_lang::prelude::*;
+use anchor_spl::{
+  associated_token::AssociatedToken, token_2022::spl_token_2022::extension::ExtensionType,
+  token_interface::{TokenInterface, TokenAccount},
+};
+use crate::account_data::{bonding_curve::BondingCurve, state::State};
+
+pub const MINT_EXTENSIONS: [ExtensionType; 1] = [ExtensionType::MetadataPointer];
+
+#[derive(Accounts)]
+#[instruction(name: String, symbol: String)]
+pub struct CreateTaxToken<'info> {
+  /// The state account of each instance of this program
+  #[account()]
+  pub state: Box<Account<'info, State>>,
+
+  /// CHECK: The Mint account of the newly created token. The initialization and extension setup will
+  /// take place in the processor function.
+  #[account(
+    init,
+    space = 10,
+    payer = token_creator,
+    seeds = [b"onlybags_token", state.key().as_ref(), format!("{}-{}", name, symbol).as_ref()],
+    bump,
+  )]
+  pub token: AccountInfo<'info>,
+
+  /// The ATA that will hold the liquidity of the curve (token side).
+  #[account(
+    init,
+    payer = token_creator,
+    associated_token::mint = token,
+    associated_token::authority = bonding_curve,
+    associated_token::token_program = token_2022,
+  )]
+  pub curve_ata: Box<InterfaceAccount<'info, TokenAccount>>,
+
+  /// The state of the bonding curve that will be used during buys and sells
+  #[account(
+    init,
+    payer = token_creator,
+    space = BondingCurve::MAX_SIZE,
+    seeds = [b"bonding_curve", state.key().as_ref(), token.key().as_ref()],
+    bump,
+  )]
+  pub bonding_curve: Box<Account<'info, BondingCurve>>,
+
+  /// The user that is creating the token
+  #[account(mut)]
+  pub token_creator: Signer<'info>,
+
+  pub associated_token_program: Program<'info, AssociatedToken>,
+  pub token_2022: Interface<'info, TokenInterface>,
+  pub system_program: Program<'info, System>,
+}
