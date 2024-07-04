992ee3462a903babe58faca1cfef957a90767dc8
ppoliani <ppoliani@gmail.com>
2024-07-04 18:46:42 +0300

    chore: update the docs

--- scripts/buy.js
+++ scripts/buy.js
@@ -15,9 +15,9 @@ const main = async () => {
   const deployer = provider.wallet.payer;
   const web3 = Web3(deployer.publicKey);
   const program = anchor.workspace.Onlybags;
-  const tokenName = "TOKEN_TAX_HUB_2";
-  const tokenSymbol= "SYMBOL_TAX_HUB_2";
-  const amount = new BN(web3.toBase("70", 9));
+  const tokenName = "Great Shibaa v100";
+  const tokenSymbol= "GreatSHiba";
+  const amount = new BN(web3.toBase("4", 7));
   const minAmountOut = new BN(0); // no slippage
   const buyer = Keypair.fromSecretKey(Buffer.from(buyerKey))
   const state = new PublicKey(config.state);
@@ -27,7 +27,7 @@ const main = async () => {
   const bondingCurve = accounts.bondingCurve(state, token, program.programId)[0];
   const wsol = constants.wsol;
   const buyerWsolAta = await web3.getAssociatedTokenAddress(wsol, buyer.publicKey);
-  const ammConfig = constants.raydiumAmmConfigDevnet;
+  const ammConfig = constants.raydiumAmmConfigMainnet;
 
   const buy_ix = await program.methods
   .buy(amount, minAmountOut)
@@ -49,7 +49,7 @@ const main = async () => {
   })
   .instruction();
 
-  const raydiumProgram = constants.raydiumProgramDevnet;
+  const raydiumProgram = constants.raydiumProgramMainnet;
   const [token0, token1] = token.toBuffer() < wsol.toBuffer() ? [token, wsol] : [wsol, token];
   const poolState = accounts.raydiumPoolState(ammConfig, token0, token1, raydiumProgram)[0];
   const [creatorToken0, creatorToken1] = token.toBuffer() < wsol.toBuffer() ? [buyerAta, buyerWsolAta] : [buyerWsolAta, buyerAta];
@@ -59,7 +59,7 @@ const main = async () => {
   ? [accounts.raydiumTokenVault(poolState, token, raydiumProgram)[0], accounts.raydiumTokenVault(poolState, wsol, raydiumProgram)[0]] 
   : [accounts.raydiumTokenVault(poolState, wsol, raydiumProgram)[0], accounts.raydiumTokenVault(poolState, token, raydiumProgram)[0]];
 
-  const createPoolFee = constants.raydiumCreatorPoolFeedDevnet;
+  const createPoolFee = constants.raydiumCreatorPoolFeedMainnet;
 
   const moveLiquidityIx = await program.methods
   .moveLiquidity()
