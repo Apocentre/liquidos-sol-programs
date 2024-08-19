56d4b4542f2fd5ee8f91abf74096744cecf0e992
ppoliani <ppoliani@gmail.com>
2024-08-19 15:45:54 +0300

    feat: add staking token to the udpate config ix

--- programs/onlybags-staking/src/lib.rs
+++ programs/onlybags-staking/src/lib.rs
@@ -49,13 +49,15 @@ pub mod onlybags_staking {
   ///
   /// * `ctx` - The Anchor context holding the accounts
   /// * `staking_duration` - The total duration of each staking pool i.e. for how long users can stake and earn rewards.
+  /// * `staking_token` - The mint account of the staking token. This will be Onlybag's token ($BAGS)
   /// * `protocol_fee` - The fee in the reward token the protocol receives.
   pub fn update_state(
     ctx: Context<UpdateState>,
     staking_duration: i64,
+    staking_token: Pubkey,
     protocol_fee: u16,
   ) -> Result<()> {
-    processors::update_state::exec(ctx, staking_duration, protocol_fee)
+    processors::update_state::exec(ctx, staking_duration, staking_token, protocol_fee)
   }
 
   /// CreatePool
--- programs/onlybags-staking/src/processors/update_state.rs
+++ programs/onlybags-staking/src/processors/update_state.rs
@@ -4,10 +4,12 @@ use crate::instructions::update_state::UpdateState;
 pub fn exec(
   ctx: Context<UpdateState>,
   staking_duration: i64,
+  staking_token: Pubkey,
   protocol_fee: u16,
 ) -> Result<()> {
   let state = &mut ctx.accounts.state;
   state.staking_duration = staking_duration;
+  state.staking_token = Some(staking_token);
   state.protocol_fee = protocol_fee;
 
   Ok(())
