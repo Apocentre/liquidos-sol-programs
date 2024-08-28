66c76feb7bcc996009a069681e5f6612da891ae3
ppoliani <ppoliani@gmail.com>
2024-08-28 13:58:50 +0300

    chore: update program error

--- programs/onlybags_swap_proxy/src/program_error.rs
+++ programs/onlybags_swap_proxy/src/program_error.rs
@@ -4,24 +4,4 @@ use anchor_lang::prelude::*;
 pub enum ErrorCode {
   #[msg("Only owner")]
   OnlyOwner,
-  #[msg("Slippage violation")]
-  SlippageViolation,
-  #[msg("Wrong treasury")]
-  WrongTreasury,
-  #[msg("Wrong token creator")]
-  WrongTokenCreator,
-  #[msg("Curve closed")]
-  CurveClosed,
-  #[msg("Curve not complete")]
-  CurveNotComplete,
-  #[msg("Invalid curve token")]
-  InvalidCurveToken,
-  #[msg("Wrong program id")]
-  WrongProgramId,
-  #[msg("Wrong instruction data")]
-  WrongIxData,
-  #[msg("Expected MoveLiquidity ix")]
-  ExpectedMoveLiquidityIx,
-  #[msg("Curve type not supported")]
-  CurveTypeNotSupported,
 }
