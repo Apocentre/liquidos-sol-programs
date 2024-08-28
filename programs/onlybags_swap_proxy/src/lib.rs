8d1803c5f63d19f5b0730674cba6949353cc7e43
ppoliani <ppoliani@gmail.com>
2024-08-28 17:02:16 +0300

    feat: add swap_base_input and swap_base_output to the entrypoint

--- programs/onlybags_swap_proxy/src/lib.rs
+++ programs/onlybags_swap_proxy/src/lib.rs
@@ -5,7 +5,7 @@ pub mod program_error;
 pub mod raydium;
 
 use anchor_lang::prelude::*;
-use crate::instructions::initialize::*;
+use crate::instructions::{initialize::*, swap::*};
 
 declare_id!("GdyU6f76XkkeWF63CqhXDVXqx56Ldva4saVxEDpWJiaY");
 
@@ -20,7 +20,6 @@ pub mod onlybags_swap_proxy {
   /// * `ctx` - The Anchor context holding the accounts
   /// * `treasury` - The treasury account that receives fees
   /// * `protocol_fee` - Current protocol fees i.e. fees collected on each swap
-
   pub fn initialize(
     ctx: Context<Initialize>,
     treasury: Pubkey,
@@ -30,6 +29,44 @@ pub mod onlybags_swap_proxy {
       ctx,
       treasury,
       protocol_fee_bps,
+    )
+  }
+
+  /// SwapBaseInput
+  ///
+  /// # Arguments
+  ///
+  /// * `ctx` - The Anchor context holding the accounts
+  /// * `amount_in` - The amount of base token to sell
+  /// * `minimum_amount_out` - minimum amount of the out token to receive
+  pub fn swap_base_input(
+    ctx: Context<Swap>,
+    amount_in: u64,
+    minimum_amount_out: u64,
+  ) -> Result<()> {
+    processors::swap_base_input::exec(
+      ctx,
+      amount_in,
+      minimum_amount_out,
+    )
+  }
+
+  /// SwapBaseOutput
+  ///
+  /// # Arguments
+  ///
+  /// * `ctx` - The Anchor context holding the accounts
+  /// * `max_amount_in` - The max amount of tokens to be sold
+  /// * `amount_out_less_fee` - The amount of tokens user wants to buy
+  pub fn swap_base_output(
+    ctx: Context<Swap>,
+    max_amount_in: u64,
+    amount_out_less_fee: u64,
+  ) -> Result<()> {
+    processors::swap_base_output::exec(
+      ctx,
+      max_amount_in,
+      amount_out_less_fee,
     )
   }
 }
