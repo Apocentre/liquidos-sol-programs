52c865d007c3f8dfb0f00e5ae35bcc1431adaadd
ppoliani <ppoliani@gmail.com>
2024-10-16 12:51:37 +0300

    feat: update release pending to account for timelock_ts

--- programs/onlybags-staking/src/processors/init_user_info.rs
+++ programs/onlybags-staking/src/processors/init_user_info.rs
@@ -1,13 +1,9 @@
 use anchor_lang::prelude::*;
 
-use crate::{
-  instructions::init_user_info::InitUserInfo, program_error::ErrorCode,
-};
+use crate::instructions::init_user_info::InitUserInfo;
 
 pub fn exec(ctx: Context<InitUserInfo>) -> Result<()> {
   let user_info = &mut ctx.accounts.user_info;
-  require!(!user_info.initialized, ErrorCode::UserInfoInitialized);
-
   user_info.initialized = true;
 
   Ok(())
