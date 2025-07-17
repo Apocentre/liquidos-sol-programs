a18aff4a752f898849f01c43dcb47fd976407a76
ppoliani <ppoliani@gmail.com>
2025-07-17 15:27:20 +0300

    fix: compiler issues

--- programs/onlybags/src/account_data/bonding_curve.rs
+++ programs/onlybags/src/account_data/bonding_curve.rs
@@ -150,7 +150,7 @@ impl BondingCurve {
 #[cfg(test)]
 mod tests {
   use anchor_lang::solana_program::pubkey::Pubkey;
-  use anchor_spl::token_2022::spl_token_2022::solana_zk_token_sdk::curve25519::scalar::Zeroable;
+use bytemuck::Zeroable;
 
   use super::BondingCurve;
 
