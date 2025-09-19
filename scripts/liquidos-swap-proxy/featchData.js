e6bb7f86537c19db823233e2be6dd2a810f32d2f
ppoliani <ppoliani@gmail.com>
2025-09-19 14:15:35 +0300

    chore: add fetch data script for proxy contract

---
+++ scripts/liquidos-swap-proxy/featchData.js
@@ -0,0 +1,29 @@
+import * as anchor from "@coral-xyz/anchor";
+import Web3Pkg from "@apocentre/solana-web3";
+import {provider} from "../helpers/provider.js";
+import * as accounts from "../helpers/accounts.js";
+import * as constants from "../helpers/constants.js";
+import raydiumIDL from "./raydium_idl_devnet.json" with { type: "json" };
+
+const Web3 = Web3Pkg.default;
+const {PublicKey} = anchor.web3
+
+const main = async () => {    
+  const deployer = provider.wallet.payer;
+  const web3 = Web3(deployer.publicKey);
+  const token = new PublicKey("GQvnQnVVNdYsX1puzL5MAgoWtg7Bs4Toh5PQsNbyYwif")
+
+  const raydiumProgram = constants.raydiumProgramDevnet;
+  const ammConfig = constants.raydiumAmmConfigDevnet;
+  const wsol = constants.wsol;
+  const [token0, token1] = token.toBuffer() < wsol.toBuffer() ? [token, wsol] : [wsol, token];
+
+  const raydiumCpmmProgram = await web3.createProgram(raydiumIDL);
+  const poolState = accounts.raydiumPoolState(ammConfig, token0, token1, raydiumProgram)[0];
+  const poolStateData = await raydiumCpmmProgram.account.poolState.fetch(poolState);
+  console.log(">>>>>>>>>>>", poolStateData);
+}
+
+main()
+.then(() => console.log("Success"))
+.catch(error => console.log("Error: ", error))
